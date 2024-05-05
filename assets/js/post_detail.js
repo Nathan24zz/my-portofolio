d79a821cbe467343999566c0251469e668811b58
Nathanael Hutama Harsono <nathanael.hutama24@gmail.com>
2024-05-05 18:39:07 +0700

    refactor: render detail of the project from json data

--- assets/js/post_detail.js
+++ assets/js/post_detail.js
@@ -4,11 +4,27 @@ var a = sessionStorage.getItem('code');
 fetch("./assets/data/post_data.json")
     .then(res => res.json())
     .then(data => {
-        console.log(data.length);
-        const container = document.querySelector('body #wrapper #main .posts');
+        const container = document.querySelector('body #wrapper #main .post');
+
         for (var i = 0; i < data.length; ++i) {
             if (a == data[i]["detail_project"]) {
-                console.log(data[i].detail_project);
+                var result = data[i];
+                const content = `
+                    <header class="major">
+                        <span class="date">">${result.date}</span>
+                        <h1>${result.title}</h1>
+                        <p>${result.detail_desc_1}</p>
+                    </header>
+                    <div class="image main"><img src=${result.image} alt="" /></div>
+                    <p>${result.detail_desc_2}</p>
+        
+                    <!-- change not a link -->
+                    <a class="button primary small">Tech</a> 
+                    ${result.tech}
+                    <br>
+                    <a href=${result.detail_project} class="button small">Github</a>
+                `
+                container.innerHTML += content;
                 break;
             }
         }
